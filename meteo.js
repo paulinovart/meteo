a462bad36e60196a5e73d23cf734ac7db4eaaa1c
paulinovart <pauline.dierick@gmail.com>
2025-02-19 14:21:17 +0100

    Update meteo.js

--- meteo.js
+++ meteo.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const apiKey = process.env.API_KEY;
         const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
         const searchBox = document.querySelector(".search input")
